07c44b40acb2c6727d901b74469b1246bb6e582a
SomeBodyLite <78022230+SomeBodyLite@users.noreply.github.com>
2021-03-23 16:35:19 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $("#app").vide({
+    mp4: "assets/video/video.mp4",
+    webm: "assets/video/video.webm",
+    ogv: "assets/video/video.ogv",
+    poster: "assets/images/cover-image.jpg",
+  });
+});
